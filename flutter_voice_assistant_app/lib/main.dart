d561daac9775716e4438a40d026c0b131d946ede
hilalahmed07 <ahmedhilal818@gmail.com>
2023-11-02 10:14:09 +0500

    here is full code

---
+++ flutter_voice_assistant_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_voice_assistant_app/screens/home_screen.dart';
+import 'package:flutter_voice_assistant_app/widgets/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Allen',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        )
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ flutter_voice_assistant_app/lib/secrets/secrets.dart
@@ -0,0 +1,2 @@
+//const openAIAPIKey = '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';
+const openAIAPIKey = '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';
